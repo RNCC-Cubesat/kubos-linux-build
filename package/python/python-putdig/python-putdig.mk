03a485fb9722c0ce227ff4817335d2a3fc636565
James Womack <womackrjames@gmail.com>
2020-08-13 14:54:56 -0700

    Updating PuTDIG version to pull fixed injection code.

--- package/python/python-putdig/python-putdig.mk
+++ package/python/python-putdig/python-putdig.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_PUTDIG_VERSION = 0.0.1
+PYTHON_PUTDIG_VERSION = 0.0.2
 PYTHON_PUTDIG_SOURCE = PuTDIG-$(PYTHON_PUTDIG_VERSION).tar.gz
-PYTHON_PUTDIG_SITE = https://files.pythonhosted.org/packages/85/c5/7a4f68164671e8877c381b19481b8aa478374c22627ff5609c1d2b4534fb
+PYTHON_PUTDIG_SITE = https://files.pythonhosted.org/packages/83/13/8f5103e8811e96286c8672914978125a77cd3813982b50841e1cdfb9dfab
 PYTHON_PUTDIG_SETUP_TYPE = setuptools
 PYTHON_PUTDIG_LICENSE = MIT
 
